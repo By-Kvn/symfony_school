9d85197ebc5bd84e3e469570ec4dc9f7c90c4ace
By-Kvn <kevin.labatte0@gmail.com>
2025-06-14 19:56:55 +0200

    feat(*): CRUD

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -1,43 +1,81 @@
 <?php
+
 namespace App\Controller;
 
+use App\Entity\Produit;
+use App\Form\ProduitForm;
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\ORM\EntityManagerInterface;
-use App\Form\ProduitTypeForm;
-use App\Entity\Produit;
+use Symfony\Component\Routing\Attribute\Route;
 
-class ProduitController extends AbstractController
+#[Route('/produit')]
+final class ProduitController extends AbstractController
 {
-    #[Route('/produit', name: 'app_produit')]
-    public function index(EntityManagerInterface $entityManager): Response
+    #[Route(name: 'app_produit_index', methods: ['GET'])]
+    public function index(ProduitRepository $produitRepository): Response
     {
-        $produits = $entityManager->getRepository(Produit::class)->findAll();
-
         return $this->render('produit/index.html.twig', [
-            'produits' => $produits,
+            'produits' => $produitRepository->findAll(),
         ]);
     }
 
-    #[Route('/produit/new', name: 'produit_new')]
-    public function new(Request $request, EntityManagerInterface $em): Response
+    #[Route('/new', name: 'app_produit_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $produit = new Produit();
-        $form = $this->createForm(ProduitTypeForm::class, $produit);
-
+        $form = $this->createForm(ProduitForm::class, $produit);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $em->persist($produit);
-            $em->flush();
+            $entityManager->persist($produit);
+            $entityManager->flush();
 
-            return $this->redirectToRoute('app_produit');
+            return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('produit/new.html.twig', [
-            'form' => $form->createView(),
+            'produit' => $produit,
+            'form' => $form,
         ]);
+    }
+
+    #[Route('/{id}', name: 'app_produit_show', methods: ['GET'])]
+    public function show(Produit $produit): Response
+    {
+        return $this->render('produit/show.html.twig', [
+            'produit' => $produit,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_produit_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Produit $produit, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ProduitForm::class, $produit);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('produit/edit.html.twig', [
+            'produit' => $produit,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_produit_delete', methods: ['POST'])]
+    public function delete(Request $request, Produit $produit, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$produit->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($produit);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_produit_index', [], Response::HTTP_SEE_OTHER);
     }
 }
